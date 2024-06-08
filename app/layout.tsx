e435fcf1e65f6f0ae34bad0829b8e584ca28e921
abror3733 <abrormurodov01.gmail.com>
2024-06-08 05:09:55 +0500

    Initial commit from Create Next App

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Green Shop",
+  description: "Green Shop by N48",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head></head>
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
